8c69e07e4468c327a2fa47e4e1a5e4e5a4639393
Tiago Pala <tiago_pala@outlook.com>
2021-06-05 16:20:09 -0300

    Refatoração da MainController para utilização de Custom Reponses

--- src/RestAspNetCoreWepApi/Controllers/MainController.cs
+++ src/RestAspNetCoreWepApi/Controllers/MainController.cs
@@ -1,18 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+﻿using Api.Business.Interfaces;
+using Api.Business.Notifications;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Api.Application.Controllers
 {
     [ApiController]
     public abstract class MainController : ControllerBase
     {
-        // Validações de notificações
+        private readonly INotifier _notifier;
+        
+        public MainController(INotifier notifier)
+        {
+            _notifier = notifier;
+        }
 
-        // Validação de modelState
+        protected bool OperacaoValida()
+        {
+            return !_notifier.TemNotificacao();
+        }
 
-        // Validação da operação de negócios
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(new
+                {
+                    Success = true,
+                    Data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                Success = false,
+                Erros = _notifier.ObterNotificacao().Select(n => n.Mensagem)
+            });
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid) 
+                NotificarErroModelStateInvalida(modelState);
+
+            return CustomResponse();
+        }
+
+        protected void NotificarErroModelStateInvalida(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+
+            foreach (var erro in erros)
+            {
+                var mensagemErro = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+
+                NotificarErro(mensagemErro);
+            }
+        }
+
+        protected void NotificarErro(string mensagem)
+        {
+            _notifier.Handle(new Notification(mensagem));
+        }
     }
 }
